b5d68fc2e13d039215c64efb0483371c0851c4db
dkulikovsky <dkulikovsky@yandex-team.ru>
2014-08-14 15:54:46 +0400

    renamed package

--- debian/cacher.cron.d
+++ debian/cacher.cron.d
@@ -1,6 +1,6 @@
 # main index locks main lock
-*/5 * * * * root /usr/local/bin/index_main.sh >> /var/log/cacher/index-main.log 2>>/var/log/cacher/index-main.err 
+*/5 * * * * root /usr/local/bin/index_main.sh >> /var/log/graphite-ch-cacher/index-main.log 2>>/var/log/graphite-ch-cacher/index-main.err 
 # update index locks main and update locks
-#1 5 * * * root /usr/local/bin/index_update.sh >> /var/log/cacher/index-update.log 2>>/var/log/cacher/index-update.err 
+#1 5 * * * root /usr/local/bin/index_update.sh >> /var/log/graphite-ch-cacher/index-update.log 2>>/var/log/graphite-ch-cacher/index-update.err 
 # delta index locks update lock
-#*/3 * * * * root /usr/local/bin/index_delta.sh >> /var/log/cacher/index-delta.log 2>>/var/log/cacher/index-delta.err 
+#*/3 * * * * root /usr/local/bin/index_delta.sh >> /var/log/graphite-ch-cacher/index-delta.log 2>>/var/log/graphite-ch-cacher/index-delta.err 
