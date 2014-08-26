c6e1d949db30df37fdc98569db873c7db8b1bffe
dkulikovsky <dkulikovsky@yandex-team.ru>
2014-08-26 18:11:55 +0400

    moved all bogus stuff to graphite-ch-conf

--- debian/graphite-ch-cacher.cron.d
+++
@@ -1,6 +0,0 @@
-# main index locks main lock
-*/5 * * * * root /usr/local/bin/index_main.sh >> /var/log/graphite-ch-cacher/index-main.log 2>>/var/log/graphite-ch-cacher/index-main.err 
-# update index locks main and update locks
-#1 5 * * * root /usr/local/bin/index_update.sh >> /var/log/graphite-ch-cacher/index-update.log 2>>/var/log/graphite-ch-cacher/index-update.err 
-# delta index locks update lock
-#*/3 * * * * root /usr/local/bin/index_delta.sh >> /var/log/graphite-ch-cacher/index-delta.log 2>>/var/log/graphite-ch-cacher/index-delta.err 
